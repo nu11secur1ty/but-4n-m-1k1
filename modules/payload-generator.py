57a54010f6e209ee1783df923f94c083c5dacb63
nu11secur1ty <venvaropt@gmail.com>
2022-09-02 17:45:15 +0300

    Update payload-generator.py

--- modules/payload-generator.py
+++ modules/payload-generator.py
@@ -5,7 +5,7 @@ import os
 
 exploit_list = ["peers","meers","#&%$*FYFDHGD","rewybtrtrt--twet","Phoenix",
             "Philadelphia", "sDSADAFDG", "ASDSADFFDF","SADSADADSADASDA",
-            "pwnedhost.com-wordpress-20220901-061314-k0mgu7.wpress"]
+            "kurecimalidalapate"]
 
 for eba4 in range(len(exploit_list)):
-    print("http://pwnedhost.com/wordpress/wp-content/ai1wm-backups/"+exploit_list[eba4])
+    print("http://your_domain/your/app/"+exploit_list[eba4])
